07559b5aa80adbce28a9db0dee4bda570217aef2
narokuu <theofficialnarokuu@gmail.com>
2023-02-21 21:31:26 -0500

    Added Windows Autopilot script

---
+++ powershell/getwindowsautopilotinfo.ps1
@@ -0,0 +1,6 @@
+New-Item -Type Directory -Path "C:\HWID"
+Set-Location -Path "C:\HWID"
+$env:Path += ";C:\Program Files\WindowsPowerShell\Scripts"
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned
+Install-Script -Name Get-WindowsAutopilotInfo
+Get-WindowsAutopilotInfo -OutputFile AutopilotHWID.csv
