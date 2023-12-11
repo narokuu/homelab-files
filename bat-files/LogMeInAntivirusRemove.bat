03d99b0c60dd73a57dae3262f3795d1f56616c41
Narokuu <32495004+narokuu@users.noreply.github.com>
2023-12-11 12:56:26 -0500

    Update LogMeInAntivirusRemove.bat

--- bat-files/LogMeInAntivirusRemove.bat
+++ bat-files/LogMeInAntivirusRemove.bat
@@ -1,4 +1,1 @@
-@echo off
-FOR /F "skip=2 tokens=2,*" %%A IN ('reg.exe query "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Endpoint Security" /v "UninstallString"') DO SET "UninstallString=%%B"
-%UninstallString% /silent /remove
-shutdown.exe /r /norestart /t 10
+
